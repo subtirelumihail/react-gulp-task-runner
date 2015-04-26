c36443b88256878373b818425536faf19525edc0
Subtirelu Mihail <dazzzz2007@gmail.com>
2015-04-27 00:47:24 +0300

    Add inline css

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,8 +8,8 @@ module.exports = function(gulp) {
   gulp.task('watch', function() {
     gulp.watch(config.source.html,  ['html',browserSync.reload]);
     gulp.watch(config.source.img,   ['img',browserSync.reload]);
-    gulp.watch(config.source.sass,  ['sass',browserSync.reload]);
-    gulp.watch(config.source.svg,   ['svg',browserSync.reload]);
+    gulp.watch(config.source.sass,  ['sass', 'html', browserSync.reload]);
+    gulp.watch(config.source.svg,   ['svg', 'html', browserSync.reload]);
     gulp.watch(config.source.font,  ['fonts',browserSync.reload]);
     gulp.watch(config.source.browserify, ['browserify',browserSync.reload]);
   });
